6c66c7f624452bc3a23ce8a2914f15d16baa0a45
sufiansaeed <sufian.saeed@wanclouds.net>
2021-10-18 22:31:21 +0500

    resolved comments

--- models/user_test.go
+++ models/user_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_user_Map(t *testing.T) {
+func TestUser_Map(t *testing.T) {
 	type fields struct {
 		ID         string
 		FatherName string
@@ -21,11 +21,33 @@ func Test_user_Map(t *testing.T) {
 		fields fields
 		want   map[string]interface{}
 	}{
-		// TODO: Add test cases.
+		{
+			name: "successful test case",
+			fields: fields{
+				ID: "123",
+				FatherName: "Saeed",
+				FirstName: "Sufian",
+				LastName: "Saeed",
+				Gender: "Male",
+				CNIC: "13101-5418171-5",
+				Email: "sufiansaeed97@gmail.com",
+				Password: "123456789",
+			},
+			want: map[string]interface{}{
+				"id": "123",
+				"father_name": "Saeed",
+				"first_name": "Sufian",
+				"last_name": "Saeed",
+				"gender": "Male",
+				"cnic": "13101-5418171-5",
+				"email": "sufiansaeed97@gmail.com",
+				"password": "123456789",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &user{
+			s := &User{
 				ID:         tt.fields.ID,
 				FatherName: tt.fields.FatherName,
 				FirstName:  tt.fields.FirstName,
@@ -42,7 +64,7 @@ func Test_user_Map(t *testing.T) {
 	}
 }
 
-func Test_user_Names(t *testing.T) {
+func TestUser_Names(t *testing.T) {
 	type fields struct {
 		ID         string
 		FatherName string
@@ -58,11 +80,24 @@ func Test_user_Names(t *testing.T) {
 		fields fields
 		want   []string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Success!",
+			fields: fields{
+				ID: "123",
+				FatherName: "Saeed",
+				FirstName: "Sufian",
+				LastName: "Saeed",
+				Gender: "Male",
+				CNIC: "13101-5418171-5",
+				Email: "sufiansaeed97@gmail.com",
+				Password: "123456789",
+			},
+			want: []string{"id", "father_name", "first_name", "last_name", "gender", "cnic", "email", "password"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &user{
+			u := &User{
 				ID:         tt.fields.ID,
 				FatherName: tt.fields.FatherName,
 				FirstName:  tt.fields.FirstName,
@@ -72,7 +107,7 @@ func Test_user_Names(t *testing.T) {
 				Email:      tt.fields.Email,
 				Password:   tt.fields.Password,
 			}
-			if got := s.Names(); !reflect.DeepEqual(got, tt.want) {
+			if got := u.Names(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Names() = %v, want %v", got, tt.want)
 			}
 		})
